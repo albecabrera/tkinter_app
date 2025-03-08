1288d76e4b732064aac7eaf222225f9ca89f08d6
albecabrera <albe.cabrera@yahoo.de>
2025-03-08 22:49:10 +0100

    cambio9

---
+++ d001.py
@@ -0,0 +1,6 @@
+frase_bienvenida = "Primer día de Python"
+print(frase_bienvenida)
+
+nombre = input("Por favor, introduzca su nombre:\n")
+
+print(nombre)
---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+# primer ejercicio
+saludo = "Hola, le voy a realizar algunas preguntas para conocerlo mejor"
+print(saludo)
+nombre = input("Introduzca su nombre:") # imprime Quique
+print(nombre)
+edad = input("Introduzca su edad:") # imprime 30
+print(edad)
+pais = input("Introduzca su país de nacimiento:") # imprime España
+print(pais)
+print(f"Hola {nombre}. Usted tiene {edad} años y nació en {pais}."  ) # imprime (Hola Quique. Usted tiene 30 años y nació en España.)
