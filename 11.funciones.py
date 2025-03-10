0f8ec9c97e0d26790bf9245311e8d2b4c2b8de9c
albecabrera <albe.cabrera@yahoo.de>
2025-03-10 22:21:33 +0100

    funciones

---
+++ 11.funciones.py
@@ -0,0 +1,9 @@
+# Como funcionan las funciones en Python
+
+def saludar(nombre, edad):
+    print(f"Tu nombre es: {nombre} y tu edad es: {edad}")
+saludar("Alberto", 47)
+saludar("Bijirita", 15)
+saludar("Alfredo", 17)
+saludar("Mati", 9) 
+saludar("Cordula", 49)
