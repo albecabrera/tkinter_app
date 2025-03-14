341edface9dfee4be69e7e06167c6ee23783c4a0
albecabrera <albe.cabrera@yahoo.de>
2025-03-15 00:04:02 +0100

    ejercicio_practico

---
+++ 24.ejercicio_practico.py
@@ -0,0 +1,21 @@
+# crear una clase llamada motocicleta
+class Motocicleta():
+    
+    # Atributos de clase
+    estado = "nuevo"
+    motor = True
+  
+    # Métodos
+    def __init__(self):
+        self.color = "rojo"
+        self.matricula = "ABC123"
+        self.combustible_litros = 5
+        self.numero_ruedas = 4
+        self.marca = "Honda" 
+        self.modelo = 250
+        self.fecha_fabricacion = 2010
+        self.velocidad_punta = 200
+        self.peso = 150
+        
+        
+ 
